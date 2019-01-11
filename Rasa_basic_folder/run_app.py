d039aa51fbb5f4d1dcd3e7b03587dd7003b5cd43
hrishikesh48 <hrishikesh.ramnath.desai@gmail.com>
2019-01-11 10:53:12 +0530

    adding code for enabling slack app

--- Rasa_basic_folder/run_app.py
+++ Rasa_basic_folder/run_app.py
@@ -7,9 +7,9 @@ from rasa_slack_connector import SlackInput
 nlu_interpreter = RasaNLUInterpreter('./models/nlu/default/restaurantnlu')
 agent = Agent.load('./models/dialogue', interpreter = nlu_interpreter)
 
-input_channel = SlackInput('xoxp-359418684578-360311923670-373479391381-faf4e29bddac4cd131221b8cf0ffa627', #app verification token
-							'xoxb-359418684578-372796513233-gHxeV6CHQFl8MPkZVrPHBsnQ', # bot verification token
-							'7uAoXNmNANaqCa9WpALV9EhR', # slack verification token
+input_channel = SlackInput('xoxp-514713642004-514881404818-515940346263-24b03666d3a7cbd1e60558089993b7ef', #app verification token
+							'xoxb-514713642004-514782809987-t8OYq1Bdfy3rdFSVGeXnok86', # bot verification token
+							'fUOT7btJnfJcWInqqP8OgwYC', # slack verification token
 							True)
 
 agent.handle_channel(HttpInputChannel(5004, '/', input_channel))
